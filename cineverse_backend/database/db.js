adc5580f048296baef29315c66f0953cdfb4c47f
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-07 20:07:56 +0545

    added init for better management

--- cineverse_backend/database/db.js
+++ cineverse_backend/database/db.js
@@ -12,35 +12,4 @@ const sequelize = new Sequelize(
   }
 );
 
-const testConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-
-const syncDatabase = async () => {
-  try {
-    // Import models here to ensure they are registered with Sequelize before synchronization
-    require("../model/userModel");
-    require("../model/movieModel");
-    require("../model/bookingModel");
-
-    await sequelize.sync({ alter: true }); // Use { alter: true } to avoid dropping tables, or { force: true } if you want to drop and recreate tables
-    console.log("Database synchronized successfully.");
-  } catch (error) {
-    console.error("Error synchronizing database:", error);
-  }
-};
-
-// Test connection and sync database
-const initializeDatabase = async () => {
-  await testConnection();
-  await syncDatabase();
-};
-
-initializeDatabase();
-
 module.exports = sequelize;
