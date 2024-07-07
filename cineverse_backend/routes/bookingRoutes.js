fd1bef1c195207b2d2152e657b2af30af9f25721
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-07 20:09:08 +0545

    managed route for booking

--- cineverse_backend/routes/bookingRoutes.js
+++ cineverse_backend/routes/bookingRoutes.js
@@ -1,14 +1,9 @@
-// routes/booking.js
 const express = require("express");
-const bookingController = require("../controllers/bookingController");
-const { authGuard } = require("../middleware/authGuard");
-
 const router = express.Router();
+const bookingController = require("../controllers/bookingController");
+const { authGuard } = require("../middleware/authGuard"); // Import authGuard middleware
 
-// Route to create a booking
+// Define routes
 router.post("/create", authGuard, bookingController.createBooking);
-
-// Route to get bookings by user
-router.get("/user", authGuard, bookingController.getBookingsByUser);
 
 module.exports = router;
