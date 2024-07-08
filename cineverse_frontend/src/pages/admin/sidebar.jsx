ca04bfc3b1e5e08c1c0b79ab1e0ba0680fac01b8
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-08 19:03:17 +0545

    stylized sidebar

--- cineverse_frontend/src/pages/admin/sidebar.jsx
+++ cineverse_frontend/src/pages/admin/sidebar.jsx
@@ -1,49 +1,59 @@
 // src/components/Sidebar.js
 
 import React from 'react';
+import { FaFilm, FaTachometerAlt, FaTicketAlt, FaUsers } from 'react-icons/fa'; // Example icons
 import { Link } from 'react-router-dom';
-import { FaTachometerAlt, FaUsers, FaFilm, FaTicketAlt } from 'react-icons/fa'; // Example icons
 import '../admin/style/sidebar.css';
 
 const Sidebar = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
     const handleLinkClick = (section) => {
         console.log(`Navigating to ${section}`);
     };
 
     return (
         <div className="sidebar">
-            <div className="logo-col">
-                <Link to="/" className="logo-text-link" onClick={() => handleLinkClick('home')}>
-                    <span className="logo-text">Cine<span className="logo-verse">verse</span></span>
-                </Link>
+            <div>
+                <div className="logo-col">
+                    <Link to="/" className="logo-text-link" onClick={() => handleLinkClick('home')}>
+                        <span className="logo-text">Cine<span className="logo-verse">verse</span></span>
+                    </Link>
+                </div>
+                <ul>
+                    <li>
+                        <Link to="/admin">
+                            <FaTachometerAlt className="sidebar-icon" />
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/users">
+                            <FaUsers className="sidebar-icon" />
+                            Manage Users
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/movies">
+                            <FaFilm className="sidebar-icon" />
+                            Manage Movies
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/bookings">
+                            <FaTicketAlt className="sidebar-icon" />
+                            Manage Bookings
+                        </Link>
+                    </li>
+                    {/* Add more links as needed */}
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <Link to="/admin">
-                        <FaTachometerAlt className="sidebar-icon" />
-                        Dashboard
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/admin/users">
-                        <FaUsers className="sidebar-icon" />
-                        Manage Users
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/admin/movies">
-                        <FaFilm className="sidebar-icon" />
-                        Manage Movies
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/admin/bookings">
-                        <FaTicketAlt className="sidebar-icon" />
-                        Manage Bookings
-                    </Link>
-                </li>
-                {/* Add more links as needed */}
-            </ul>
+            {user && (
+                <div className="user-section">
+                    <hr />
+                    <span>{user.name}</span> {/* Display the user's name */}
+                </div>
+            )}
         </div>
     );
 };
