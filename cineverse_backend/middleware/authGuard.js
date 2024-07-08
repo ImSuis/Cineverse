8f8fb01115a680ab723e86213aa7e30fd8a42e2a
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-08 13:14:52 +0545

    updated admin routing

--- cineverse_backend/middleware/authGuard.js
+++ cineverse_backend/middleware/authGuard.js
@@ -37,35 +37,32 @@ const authGuard = (req, res, next) => {
 };
 
 const authGuardAdmin = (req, res, next) => {
-  //get header authorization
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     console.log("Authorization header not found");
-    return res.json({
+    return res.status(403).json({
       success: false,
       message: "Authorization header not found",
     });
   }
 
-  //get token
   const token = authHeader.split(" ")[1];
   if (!token) {
     console.log("Token not found");
-    return res.json({
+    return res.status(403).json({
       success: false,
       message: "Token not found",
     });
   }
 
   try {
-    const decodeUser = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
-    console.log("Decoded User:", decodeUser);
-    req.user = decodeUser;
+    const decodedUser = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    console.log("Decoded User:", decodedUser);
+    req.user = decodedUser;
 
-    //check if user is admin
     if (!req.user.isAdmin) {
       console.log("User is not an admin");
-      return res.json({
+      return res.status(403).json({
         success: false,
         message: "Permission denied. Admin access required.",
       });
@@ -74,7 +71,7 @@ const authGuardAdmin = (req, res, next) => {
     next();
   } catch (error) {
     console.log("Invalid token:", error.message);
-    res.json({
+    return res.status(403).json({
       success: false,
       message: "Invalid token",
     });
