cbf3bb03c50f7c2fa9de07eb24a49121900abd70
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-08 13:54:45 +0545

    added remaining operations for schedule

--- cineverse_backend/controllers/scheduleController.js
+++ cineverse_backend/controllers/scheduleController.js
@@ -20,3 +20,71 @@ exports.createSchedule = async (req, res) => {
   }
 };
 
+exports.getAllSchedules = async (req, res) => {
+  try {
+    const schedules = await Schedule.findAll({
+    });
+    res.status(200).json(schedules);
+  } catch (error) {
+    console.error("Error fetching schedules:", error);
+    res.status(500).json({ message: "Error fetching schedules", error });
+  }
+};
+
+// Get schedule by ID
+exports.getScheduleById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const schedule = await Schedule.findByPk(id, {
+    });
+    if (!schedule) {
+      return res.status(404).json({ message: "Schedule not found" });
+    }
+    res.status(200).json(schedule);
+  } catch (error) {
+    console.error("Error fetching schedule by ID:", error);
+    res.status(500).json({ message: "Error fetching schedule by ID", error });
+  }
+};
+
+// Update schedule by ID
+exports.updateScheduleById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { date, MovieId, LocationId, ShowtimeId } = req.body;
+
+    const schedule = await Schedule.findByPk(id);
+    if (!schedule) {
+      return res.status(404).json({ message: "Schedule not found" });
+    }
+
+    await schedule.update({
+      date,
+      MovieId,
+      LocationId,
+      ShowtimeId
+    });
+
+    res.status(200).json(schedule);
+  } catch (error) {
+    console.error("Error updating schedule by ID:", error);
+    res.status(500).json({ message: "Error updating schedule by ID", error });
+  }
+};
+
+// Delete schedule by ID
+exports.deleteScheduleById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const schedule = await Schedule.findByPk(id);
+    if (!schedule) {
+      return res.status(404).json({ message: "Schedule not found" });
+    }
+
+    await schedule.destroy();
+    res.status(200).json({ message: "Schedule deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting schedule by ID:", error);
+    res.status(500).json({ message: "Error deleting schedule by ID", error });
+  }
+};
--- cineverse_backend/routes/scheduleRoutes.js
+++ cineverse_backend/routes/scheduleRoutes.js
@@ -5,5 +5,9 @@ const { authGuardAdmin } = require("../middleware/authGuard");
 
 // Define routes
 router.post("/create", authGuardAdmin, scheduleController.createSchedule);
+router.get("/", authGuardAdmin, scheduleController.getAllSchedules);
+router.get("/:id", authGuardAdmin, scheduleController.getScheduleById);
+router.put("/:id", authGuardAdmin, scheduleController.updateScheduleById);
+router.delete("/:id", authGuardAdmin, scheduleController.deleteScheduleById);
 
 module.exports = router;
