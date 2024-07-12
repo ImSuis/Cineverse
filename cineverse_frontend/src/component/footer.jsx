beb8d2cdf09690605864100b0b8cb51d62213089
Suyash Joshi <joshisuyash322@gmail.com>
2024-07-12 18:25:23 +0545

    removed footer from admin

--- cineverse_frontend/src/component/footer.jsx
+++ cineverse_frontend/src/component/footer.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa'; // Import icons from react-icons
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../style/footer.css'; // Custom CSS for the footer
 
 const Footer = () => {
-    // Example function for handling link click (adjust as per your routing needs)
-    const handleLinkClick = (route) => {
-        // Implement your routing logic here
-        console.log(`Navigating to: ${route}`);
-    };
-
     return (
         <div className="footer">
-            <Col className="logo-col">
-                <Link to="/" className="logo-text-link" onClick={() => handleLinkClick('home')}>
+            <div className="logo-col">
+                <Link to="/" className="logo-text-link">
                     <span className="logo-text">Cine<span className="logo-verse">verse</span></span>
                 </Link>
-            </Col>
-            
+                <span className="faq">FAQs</span>
+            </div>
+            <div className="social-col">
+                <span className="follow-text">Follow us on</span>
+                <div className="social-icons">
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram />
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebookF />
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
--- cineverse_frontend/src/main.js
+++ cineverse_frontend/src/main.js
@@ -74,7 +74,7 @@ const Main = ({
         show={showRegisterModal}
         handleClose={handleRegisterModalClose}
       />
-                  <Footer />
+      {!isAdminRoute && <Footer />}
 
     </>
   );
